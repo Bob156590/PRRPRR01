5b230fd8a11d8caf856b94ed7177024493e796b1
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-11-10 17:40:31 +0100

    Update Program.cs

--- textbasedadventure/Program.cs
+++ textbasedadventure/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace tba
 {
     class program
@@ -21,17 +21,33 @@ namespace tba
                         ehp = rand.Next(10, 21);
                         eres = rand.Next(5, 14);
                         edmg = rand.Next(5, 11);
-                        while (ehp > 0)
+                        Console.Clear();
+                        while (ehp > 0 && hp > 0)
                         {
-                            Console.Clear();
-                            Console.WriteLine($"Skeleton:\nHp: {ehp}\nRes: {eres}\nDmg: {edmg}-{edmg + 5}");
+                            Console.WriteLine($"Skeleton:\nHp: {ehp}\nRes: {eres}\nDmg: {edmg}-{edmg + 5}\n\nYou:Hp: {hp}\nRes: {res}\nDmg:{dmg}");
                             Console.WriteLine("1. attack\n2. defend\n3. run");
-                            switch (Console.Read())
+                            int hedmg = rand.Next(edmg, edmg + 5);//wanted to show how much dmg the enemy did to u. btw i did it outside the switch-case because i't less work.
+                            int cho = int.Parse(Console.ReadLine());//wanted the player to see how much dmg they did before clearing.
+                            Console.Clear();//read 
+                            switch (cho)//
                             {
-                                case 1:
-                                    ehp -= rand.Next(dmg, dmg+5);
-                                    break;                                                                                                               
-                                case 2:
+                                case 1://ReadLine is string so i did this [:).
+                                    int hdmg = rand.Next(dmg, dmg + 5);//wanted to show how much dmg u did to the enemy.
+                                    ehp -= hdmg;
+                                    Console.WriteLine($"You delt {hdmg} dmg to the Skeleton\n");
+                                    hp-= hdmg;
+                                    Console.WriteLine($"The Skeletpn delt {hedmg} dmg to you\n");
+                                    break;
+                                case 2://still work in progress.
+                                    if (res/1.1 > hedmg)
+                                    {
+                                        Console.WriteLine("You doged the attack\n");
+                                    }
+                                    else
+                                    {
+                                        hp -= hedmg;
+                                        Console.WriteLine($"you took {hedmg} dmg from the enemy\n");
+                                    }
                                     break;
                                 case 3:
                                     break;
@@ -41,11 +57,12 @@ namespace tba
                         }
                         break;
                     case "knight":
-                        break ;
+                        break;
                     default:
                         break;
 
                 }
+                Console.WriteLine("You died dumbass");
             }
         }
     }
